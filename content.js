1a6afe5aeda2d0455e7f6fcad1248321948a3dcd
joeshakely <joeshakely@gmail.com>
2023-10-28 12:19:29 -0700

    ok

--- content.js
+++ content.js
@@ -1,31 +1,39 @@
-const styleSheets = Array.from(document.styleSheets)
+(function() {
+    const styleSheets = Array.from(document.styleSheets);
+    let cssText = '';
 
-let cssText = ''
-
-styleSheets.forEach((styleSheet) => {
-    try {
+    styleSheets.forEach((styleSheet) => {
+      try {
         if (styleSheet.cssRules) {
-            const cssRules = Array.from(styleSheet.cssRules)
-            cssRules.forEach((rule) => {
-                cssText += rule.cssText + '\n'
-            })
+          const cssRules = Array.from(styleSheet.cssRules);
+          cssRules.forEach((rule) => {
+            cssText += rule.cssText + '\n';
+          });
         }
-    } catch (e) {
-        console.log("Can't read the css rules of: " + styleSheet.href, e)
-    }
-})
-let pageDataText = '<style>\n' + cssText + '\n</style>\n';
-pageDataText += document.documentElement.outerHTML;
+      } catch (e) {
+        console.log("Can't read the css rules of: " + styleSheet.href, e);
+      }
+    });
 
-const blob = new Blob([pageDataText], { type: 'text/txt;charset=utf-8;' })
+    const scriptTags = document.querySelectorAll('script');
+    scriptTags.forEach((scriptTag) => {
+      if (scriptTag.textContent.includes('.set(')) {
+        scriptTag.remove();
+      }
+    });
 
-// Create a download link and click it
-const link = document.createElement('a')
-const url = URL.createObjectURL(blob)
-link.setAttribute('href', url)
-link.setAttribute('download', 'page.html')
-document.body.appendChild(link)
-link.click()
-// Cleanup
-document.body.removeChild(link)
-URL.revokeObjectURL(url)
+    let pageDataText = '<style>\n' + cssText + '\n</style>\n';
+    pageDataText += document.documentElement.outerHTML;
+
+    const blob = new Blob([pageDataText], { type: 'text/txt;charset=utf-8;' });
+
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'page.html');
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  })();
